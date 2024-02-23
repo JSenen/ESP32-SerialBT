2c7e2f821d7d3a918aa1f110d0d2da76241fdd47
Juan Senen <juansenen@hotmail.com>
2024-02-23 09:53:48 +0100

    BLE conexion

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,69 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
-#include "BluetoothSerial.h"
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
-#define DHTPIN 2  
-//#define LED 13
+#define LED_PIN 2 // Pin del LED
 
-#define DHTTYPE DHT11   // DHT 11
-#define SERVICE_UUID "00001101-0000-1000-8000-00805F9B34FB"
+BLECharacteristic *pCharacteristic;
+bool state = false;
 
-DHT dht(DHTPIN, DHTTYPE);
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      Serial.println("Conectado al cliente BLE");
+    };
 
-//Check if Bluetooth is enable
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-//Instance of BluetoothSerial
-BluetoothSerial SerialBT;
-//const int ledPin = 3; // Definimos el pin del LED
-
-//initialize Bluetooth serial comunication 
-void setup() {
-  Serial.begin(115200); //Start bluetooth module
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  dht.begin(); // Begin DHTC11 Temperature sensor
-  Serial.println("The device started, now you can pair it with bluetooth!");
- // pinMode(LED, OUTPUT); // Inicializamos el pin del LED como salida
-}
-
-//Send and receive data
-void loop() {
-  if (SerialBT.available()) {
-    // Leer los datos recibidos en un buffer
-    uint8_t buffer[32];
-    size_t size = SerialBT.readBytes(buffer, sizeof(buffer));
-
-    // Convertir los bytes recibidos en una cadena hexadecimal
-String receivedHex;
-for (size_t i = 0; i < size; i++) {
-  // Convierte el byte en su representación hexadecimal como una cadena
-  String hexByte = String(buffer[i], HEX);
-  // Asegúrate de que el byte se represente con dos caracteres
-  if (hexByte.length() == 1) {
-    hexByte = "0" + hexByte; // Agrega un 0 al principio si es necesario
-  }
-  // Concatena la representación hexadecimal del byte a la cadena recibida
-  receivedHex += hexByte;
-}
-Serial.println("HEX recibido = " + receivedHex);
-
-    // Comparar la cadena hexadecimal recibida con el código esperado
-    if (receivedHex == "0207e20103ef04") {
-      // Enviar ACK correcto
-      SerialBT.print("020606031104\r\n");
-    } else {
-      // Enviar NACK
-      SerialBT.print("020615032004\r\n");
+    void onDisconnect(BLEServer* pServer) {
+      Serial.println("Desconectado del cliente BLE");
     }
+};
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if(value.length() == 1) {
+        if(value[0] == '1') {
+          digitalWrite(LED_PIN, HIGH);
+          state = true;
+        } else if(value[0] == '0') {
+          digitalWrite(LED_PIN, LOW);
+          state = false;
+        }
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+
+  BLEDevice::init("ESP32 BLE Device");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b"));
+
+  pCharacteristic = pService->createCharacteristic(
+                      UUID("beb5483e-36e1-4688-b7f5-ea07361b26a8"),
+                      BLECharacteristic::PROPERTY_READ |
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pService->start();
+  pServer->getAdvertising()->start();
+}
+
+void loop() {
+  delay(1000);
+
+  if(state) {
+    pCharacteristic->setValue("1");
+  } else {
+    pCharacteristic->setValue("0");
   }
+
+  pCharacteristic->notify();
 }
